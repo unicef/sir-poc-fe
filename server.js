f20254d53c8906e354db8e11fe39e57519d777d7
daniel tabarcea <dani.tabarcea@gmail.com>
2018-07-09 18:39:42 +0300

    added node script for serving the built version

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express'); // eslint-disable-line
+var browserCapabilities = require('browser-capabilities'); // eslint-disable-line
+var proxy = require('http-proxy-middleware');
+
+const app = express();
+const basedir = __dirname + '/build/'; // eslint-disable-line
+
+function getSourcesPath(request) {
+  let clientCapabilities = browserCapabilities.browserCapabilities(
+      request.headers['user-agent']);
+
+  clientCapabilities = new Set(clientCapabilities); // eslint-disable-line
+
+  if (clientCapabilities.has('es2015')) {
+    return basedir + 'es6-bundled/';
+  } else {
+    return basedir + 'es5-bundled/';
+  }
+}
+app.use('/api', proxy({target: 'http://localhost:8080'}));
+
+app.get(/.*service-worker\.js/, function(req, res) {
+  res.sendFile(getSourcesPath(req) + 'service-worker.js');
+});
+
+app.get(/.*web-animations-next-lite.min\.js/, function(req, res) {
+  res.sendFile(getSourcesPath(req) + 'node_modules/web-animations-js/web-animations-next-lite.min.js');
+});
+
+app.use('/', (req, res, next) => {
+  express.static(getSourcesPath(req))(req, res, next);
+});
+
+
+app.listen(8082);

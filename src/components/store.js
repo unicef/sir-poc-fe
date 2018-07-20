fe08dfb8e84cdfdda68ab764d92ed6083cf8a74c
daniel tabarcea <dani.tabarcea@gmail.com>
2018-07-20 19:08:27 +0300

    created indexedDB adapter for redux persistance

--- src/components/store.js
+++ src/components/store.js
@@ -25,8 +25,8 @@ import errors from '../reducers/errors.js';
 
 import { persistStore, persistCombineReducers } from 'redux-persist';
 
-import storage from 'redux-persist/es/storage';
-
+// import storage from 'redux-persist/es/storage';
+import { storage } from './indexeddb-storage.js';
 // Sets up a Chrome extension for time travel debugging.
 // See https://github.com/zalmoxisus/redux-devtools-extension for more information.
 const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || origCompose;

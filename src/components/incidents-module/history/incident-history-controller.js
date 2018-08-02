0a251889cd33ab9cc6cd0a51c843556f59f10fec
daniel tabarcea <dani.tabarcea@gmail.com>
2018-08-02 19:02:09 +0300

    added offline redirect

--- src/components/incidents-module/history/incident-history-controller.js
+++ src/components/incidents-module/history/incident-history-controller.js
@@ -8,6 +8,7 @@ import { connect } from 'pwa-helpers/connect-mixin.js';
 import { store } from '../../../redux/store.js';
 
 import { makeRequest, prepareEndpoint } from '../../common/request-helper.js';
+import { updatePath } from '../../common/navigation-helper.js';
 import { Endpoints } from '../../../config/endpoints.js';
 import '../../styles/shared-styles.js';
 
@@ -134,7 +135,9 @@ export class IncidentHistory extends HistoryHelpers(connect(store)(PolymerElemen
   }
 
   _idChanged(newId) {
-    if (!this.state.app.offline && newId) {
+    if (this.state.app.offline) {
+      updatePath('/incidents/list');
+    } else if (newId) {
       this._fetchHistory();
     }
   }

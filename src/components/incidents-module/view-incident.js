ee6078f668bc6b9797edfe043b8cb05c5913aca0
Adriana Trif <trif.corina.adriana@gmail.com>
2018-08-13 14:46:25 +0300

    Navigation betwen view/edit incident and tabs update

--- src/components/incidents-module/view-incident.js
+++ src/components/incidents-module/view-incident.js
@@ -1,6 +1,9 @@
 /**
 @license
 */
+
+import '@polymer/iron-icons/editor-icons.js';
+import { html } from '@polymer/polymer/polymer-element.js';
 import { IncidentsBaseView } from './incidents-base-view.js';
 import { isOnViewIncident } from '../../reducers/app';
 
@@ -17,6 +20,27 @@ class ViewIncident extends IncidentsBaseView {
 
   isOnExpectedPage() {
     return isOnViewIncident(this.state);
+  }
+
+  static get getTitleTemplate() {
+    return html`
+      <style>
+        .nav-buttons {
+          display: inline-flex;
+          align-items: center;
+        }
+      </style>
+      <div class="layout-horizontal flex-c">
+        <div class="col-11">
+          <h2>[[title]]</h2>
+        </div>
+        <div class="col-1 nav-buttons">
+          <a href="incidents/edit/[[incidentId]]" title="Edit Incident">
+            <iron-icon icon="editor:mode-edit"></iron-icon>
+          </a>
+        </div>
+      </div>
+    `;
   }
 }
 

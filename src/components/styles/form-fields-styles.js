221ec6770c756f82cf518b5d9d55295f2e7a87f9
Adrian Hangan <iovuadrian@gmail.com>
2018-08-13 14:57:50 +0300

    field focus shadow added

--- src/components/styles/form-fields-styles.js
+++ src/components/styles/form-fields-styles.js
@@ -29,6 +29,16 @@ documentContainer.innerHTML = `<dom-module id="form-fields-styles">
         width: 100%;
       }
             
+      paper-input,
+      paper-textarea,
+      datepicker-lite,
+      etools-dropdown-lite,
+      etools-dropdown-multi-lite {
+        --paper-input-container-underline-focus: {
+          box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+        }
+      }
+      
       paper-input[readonly],
       paper-textarea[readonly],
       datepicker-lite[readonly],

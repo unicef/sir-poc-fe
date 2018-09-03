ea168e681188222095bb589e387b508da3ec252e
Daniel Tabarcea <dani.tabarcea@gmail.com>
2018-09-03 13:00:35 +0300

    Fixed incidents edit route issue

--- src/components/incidents-module/incidents-list.js
+++ src/components/incidents-module/incidents-list.js
@@ -247,10 +247,9 @@ class IncidentsList extends connect(store)(DateMixin(PaginationMixin(ListCommonM
         type: String,
         value: ''
       },
-      _isActiveModule: {
+      visible: {
         type: Boolean,
-        value: false,
-        observer: '_isActiveModuleChanged'
+        observer: '_visibilityChanged'
       },
       _moduleNavigatedFrom: {
         type: String,
@@ -265,23 +264,21 @@ class IncidentsList extends connect(store)(DateMixin(PaginationMixin(ListCommonM
     this.store = store;
   }
 
-
   _updateUrlQs() {
-
+    if (!this.visible) {
+      return false;
+    }
     this.set('_lastQueryString', this._buildQueryString());
-
     this.updateAppState('/incidents/list', this._lastQueryString, false);
   }
 
-
-  _isActiveModuleChanged(newVal, oldVal) {
+  _visibilityChanged(visible) {
     if (this._queryParamsInitComplete) {
-      if (newVal && newVal !== oldVal && this._lastQueryString !== '') {
+      if (visible && this._lastQueryString !== '') {
         this.updateAppState('/incidents/list', this._lastQueryString, false);
       }
     }
   }
-
 
   _queryParamsChanged(params) {
 
@@ -323,9 +320,6 @@ class IncidentsList extends connect(store)(DateMixin(PaginationMixin(ListCommonM
 
     this.set('_moduleNavigatedFrom', state.app.locationInfo.selectedModule);
 
-    if (typeof state.app.locationInfo.selectedModule !== 'undefined') {
-      this.set('_isActiveModule', state.app.locationInfo.selectedModule === 'incidents');
-    }
     if (typeof state.app.locationInfo.queryParams !== 'undefined') {
       this._queryParams = state.app.locationInfo.queryParams;
     }

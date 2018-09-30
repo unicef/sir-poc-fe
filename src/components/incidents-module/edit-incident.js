39d6c3eca546a1c7943db5ca6df725867dff8bc0
Adriana Trif <trif.corina.adriana@gmail.com>
2018-09-30 16:52:22 +0300

    Allow edit for attachment note

--- src/components/incidents-module/edit-incident.js
+++ src/components/incidents-module/edit-incident.js
@@ -3,7 +3,7 @@
 */
 import { IncidentsBaseView } from './incidents-base-view.js';
 import { editIncident } from '../../actions/incidents.js';
-import { makeRequest } from '../../components/common/request-helper.js';
+import { makeRequest, prepareEndpoint } from '../../components/common/request-helper.js';
 import { Endpoints } from '../../config/endpoints.js';
 import { serverError } from '../../actions/errors';
 
@@ -26,12 +26,11 @@ class EditIncident extends IncidentsBaseView {
       return;
     }
 
-   // this.saveAttachmentsNotes().then(() =>{
+    this.saveAttachmentsNotes().then(() =>{
       this.store.dispatch(editIncident(this.incident));
-   // });
+    });
   }
 
-  // TODO - update after endpoint changes
   saveAttachmentsNotes() {
     if (this.state.app.offline || this.incident.unsynced) {
       return Promise.resolve();
@@ -44,25 +43,32 @@ class EditIncident extends IncidentsBaseView {
       return Promise.resolve();
     }
 
-    let attChanges;
+    let attChanges = [];
     let origAtt = origIncident.attachments;
     let currAtt = this.incident.attachments;
 
     for (let i = 0; i < origAtt.length; i++) {
       if (origAtt[i].note !== currAtt[i].note) {
-        attChanges = { // TODO
+        attChanges.push({
           id: currAtt[i].id,
           note: currAtt[i].note
-        };
+        });
       }
     }
 
-    if (!attChanges) {
+    if (!attChanges.length) {
       return Promise.resolve();
     }
 
-    return makeRequest(Endpoints.addIncidentAttachments, attChanges).catch((err) => {
-           this.store.dispatch(serverError(err));
+    let operations = [];
+    attChanges.forEach((c) => {
+      let endpoint = prepareEndpoint(Endpoints.editIncidentAttachments, {id: c.id});
+      operations.push(makeRequest(endpoint, {note: c.note}));
+    });
+
+    return Promise.all(operations).catch((err) => {
+           this.store.dispatch(serverError(err.status === 500 ?
+             'There was an error updating Related Documents section' : err));
            return Promise.resolve(); // the rest of the Incident changes will be saved despite attachments error
           });
   }

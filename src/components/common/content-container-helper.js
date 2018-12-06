aa5b09115d3ef297f8781544e79d5e7710219e68
daniel tabarcea <dani.tabarcea@gmail.com>
2018-12-06 15:53:00 +0200

    removed testing code

--- src/components/common/content-container-helper.js
+++ src/components/common/content-container-helper.js
@@ -1,12 +1,10 @@
 const _getContentContainer = () => {
   let appShell = document.querySelector('app-shell');
   if (!appShell) {
-    console.warn('app-shell not found');
     return null;
   }
   let appHeadLayout = appShell.shadowRoot.querySelector('app-header-layout');
   if (!appHeadLayout) {
-    console.warn('app-head-layout not found');
     return null;
   }
   return appHeadLayout.shadowRoot.querySelector('#contentContainer');
@@ -15,7 +13,7 @@ const _getContentContainer = () => {
 export const scrollToTop = () => {
   const contentContainer = _getContentContainer();
   if (!contentContainer) {
-    console.warn('Cannot scroll! `contentContainer` object is null or undefined');
+    // console.warn('Cannot scroll! `contentContainer` object is null or undefined');
     return;
   }
   contentContainer.scrollTop = 0;

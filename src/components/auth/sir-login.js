dcab2c5e4d0707839fbc4dfcd738a00bf7b3e561
daniel tabarcea <dani.tabarcea@gmail.com>
2019-02-12 17:43:34 +0200

    working draft of the request access form unicef/sir-releases#353

--- src/components/auth/sir-login.js
+++ src/components/auth/sir-login.js
@@ -1,8 +1,10 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import '@polymer/iron-flex-layout/iron-flex-layout.js';
 import '@polymer/iron-icons/iron-icons.js';
+import '@polymer/paper-input/paper-input.js';
 import '@polymer/paper-button/paper-button.js';
 import { SirMsalAuth } from './jwt/msal-authentication.js';
+import { validateAllRequired } from '../common/validations-helper.js';
 
 /**
  * @customElement
@@ -66,6 +68,10 @@ class SirLogin extends PolymerElement {
           @apply --layout-center;
           text-align: center;
         }
+
+        paper-input {
+          margin-bottom: 8px;
+        }
       </style>
 
       <div class="icons">
@@ -74,17 +80,87 @@ class SirLogin extends PolymerElement {
         <iron-icon id="login-icon" icon="account-circle"></iron-icon>
       </div>
       <div id="login-area">
-        <h1>Welcome to UNICEF SIR</h1>
-        <p>Sign into your Microsoft Account</p>
-        <paper-button raised on-tap="_login">Sign In <iron-icon icon="arrow-forward"></iron-icon></paper-button>
+        <div hidden$="[[hideLoginForm]]">
+          <h1>Welcome to UNICEF SIR</h1>
+          <p>Sign into your Microsoft Account</p>
+          <paper-button raised on-tap="_login"> Sign In <iron-icon icon="arrow-forward"></iron-icon></paper-button>
+          <paper-button raised on-tap="_showRequestAccessForm"> Request Access </paper-button>
+        </div>
+        <div hidden$="[[!hideLoginForm]]">
+          <p> Tell us more about you </p>
+          <paper-input label="Full name"
+                       auto-validate
+                       placeholder="&#8212;"
+                       value="{{requester.name}}"
+                       required
+                       error-message="This is required">
+          </paper-input>
+          <paper-input label="Email"
+                       auto-validate
+                       placeholder="&#8212;"
+                       value="{{requester.email}}"
+                       required
+                       error-message="This is required">
+            <span  slot="suffix">@unicef.org</span>
+          </paper-input>
+          <paper-input label="Job title"
+                       auto-validate
+                       placeholder="&#8212;"
+                       value="{{requester.name}}"
+                       required
+                       error-message="This is required">
+          </paper-input>
+          <paper-input label="Duty station"
+                       auto-validate
+                       placeholder="&#8212;"
+                       value="{{requester.city}}"
+                       required
+                       error-message="This is required">
+          </paper-input>
+          <br>
+          <br>
+          <paper-button raised on-tap="_requestAccess"> Request access </paper-button>
+          <paper-button raised on-tap="_showLoginForm"> Back to log in </paper-button>
+        </div>
+
       </div>
     `;
+
+  }
+
+  static get properties() {
+    return {
+      hideLoginForm: {
+        type: Boolean,
+        value: false
+      },
+      requester: {
+        type: Object,
+        value: {}
+      }
+    }
+  }
+
+  _validate() {
+    return validateAllRequired(this);
+  }
+
+  _showRequestAccessForm() {
+    this.hideLoginForm = true;
+  }
+
+  _showLoginForm() {
+    this.hideLoginForm = false;
   }
 
   _login() {
     SirMsalAuth.login().then(() => {
       window.location.reload();
     });
+  }
+
+  _requestAccess() {
+    console.log('Request access, show me a success message. Then take me back to the login form.');
   }
 }
 

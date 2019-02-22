b4f78d1ac2e8826a56778db66e95a813a0321bbf
daniel tabarcea <dani.tabarcea@gmail.com>
2019-02-22 19:28:07 +0200

    code clean-up, added checks, disabled redux persist if no service worker possible

--- service-worker.js
+++ service-worker.js
@@ -11,5 +11,3 @@
 console.info(
   'Service worker disabled for development, will be generated at build time.'
 );
-
-importScripts("sw.js");
--- sw.js
+++ sw.js
@@ -8,50 +8,49 @@ self.addEventListener('activate', function(event) {
   event.waitUntil(self.clients.claim()); // Become available to all pages
 });
 
+if (indexedDB) {
+  let db;
+  let idb = indexedDB.open('sir-poc', 1);
+  let lastActivityTimestamp = getCurrentTimestamp();
 
-let store, db;
-let idb = indexedDB.open('sir-poc', 1);
-let lastActivityTimestamp = getCurrentTimestamp();
+  idb.onupgradeneeded = () => {
+    db = idb.result;
+    db.createObjectStore('PersistedState', {keyPath: 'key'});
+  };
 
-idb.onupgradeneeded = () => {
-  db = idb.result;
-  store = db.createObjectStore('PersistedState', {keyPath: 'key'});
-};
+  idb.onerror = () => {
+    console.warn('IDB creation failed');
+    reject();
+  };
 
-idb.onerror = () => {
-  console.warn('IDB creation failed');
-  reject();
-};
-
-function createTransaction() {
-  let tx = idb.result.transaction('PersistedState', 'readwrite');
-  return tx.objectStore('PersistedState');
-}
-
-function clearSavedState() {
-  createTransaction().clear();
-}
-
-function getCurrentTimestamp() {
-  return (new Date()).getTime();
-}
-
-function getElapsedTime(timestamp) {
-  return getCurrentTimestamp() - timestamp;
-}
-
-self.addEventListener('message', (event) => {
-  lastActivityTimestamp = getCurrentTimestamp();
-});
-
-// check to see if we need to erase the saved state
-let interval = setInterval(() => {
-  if (getElapsedTime(lastActivityTimestamp) >= ERASE_DATABASE_AFTER) {
-    clearSavedState();
-    lastActivityTimestamp = getCurrentTimestamp();
-    console.log('persisted state deleted due to inactivity');
-  } else {
-    console.log('last activity was', getElapsedTime(lastActivityTimestamp) / 1000, 'seconds ago')
+  function createTransaction() {
+    let tx = idb.result.transaction('PersistedState', 'readwrite');
+    return tx.objectStore('PersistedState');
   }
-}, CHECK_IDLE_STATE_INTERVAL);
+
+  function clearSavedState() {
+    createTransaction().clear();
+  }
+
+  function getCurrentTimestamp() {
+    return (new Date()).getTime();
+  }
+
+  function getElapsedTime(timestamp) {
+    return getCurrentTimestamp() - timestamp;
+  }
+
+  self.addEventListener('message', () => {
+    lastActivityTimestamp = getCurrentTimestamp();
+  });
+
+  // check to see if we need to erase the saved state
+  setInterval(() => {
+    if (getElapsedTime(lastActivityTimestamp) >= ERASE_DATABASE_AFTER) {
+      clearSavedState();
+      lastActivityTimestamp = getCurrentTimestamp();
+      console.log('db deleted due to inactivity');
+    }
+  }, CHECK_IDLE_STATE_INTERVAL);
+}
 
